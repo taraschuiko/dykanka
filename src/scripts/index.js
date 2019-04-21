c58f02c7ec5c416b93214aa77b3ebe8683f869c2
taraschuiko <taraschuiko@gmail.com>
2019-04-21 17:03:59 +0300

    Made menu mobile friendly

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,12 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+var mobileMenuIcon = document.querySelector(".nav__mobile-icon img");
+var mobileMenu = document.querySelector(".nav__menu");
+
+mobileMenuIcon.addEventListener("click", () => {
+  if (!mobileMenu.classList.contains("nav__menu_active")) {
+    mobileMenu.classList.add("nav__menu_active");
+  } else {
+    mobileMenu.classList.remove("nav__menu_active");
+  }
+});
