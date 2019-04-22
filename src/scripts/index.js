df112808176af759cdf337985208ef86f30264ae
taraschuiko <taraschuiko@gmail.com>
2019-04-22 21:01:14 +0300

    Added mobile menu close button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,10 +3,16 @@ import '../styles/index.scss';
 var mobileMenuIcon = document.querySelector(".nav__mobile-icon img");
 var mobileMenu = document.querySelector(".nav__menu");
 
+var menuOpenIcon = "./assets/svg/menu_1.svg";
+var menuCloseIcon = "./assets/svg/close_1.svg";
+
 mobileMenuIcon.addEventListener("click", () => {
+  console.log(mobileMenuIcon.attributes);
   if (!mobileMenu.classList.contains("nav__menu_active")) {
     mobileMenu.classList.add("nav__menu_active");
+    mobileMenuIcon.attributes[0].value = menuCloseIcon;
   } else {
     mobileMenu.classList.remove("nav__menu_active");
+    mobileMenuIcon.attributes[0].value = menuOpenIcon;
   }
 });
